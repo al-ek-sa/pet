c7ecae2a45295e0a9654a84d7ad063033f63cded
al-ek-sa <alexandra.lishik@gmail.com>
2026-08-03 23:11:45 +0300

    feat: add authentication flow UI (login + register + verify)

---
+++ front/js/vendor/auth_client.js
@@ -0,0 +1,6 @@
+function goToRegistrationCode(){
+  window.location.href="../../html/registration_code.html";
+}
+function goToRegisterLabel(){
+  window.location.href = "../../html/registration_label.html";
+}

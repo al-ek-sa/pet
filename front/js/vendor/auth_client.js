7b4b2b3bf40150ddc9f214369485d380e8107a4a
al-ek-sa <alexandra.lishik@gmail.com>
2026-08-06 20:53:28 +0300

    feat: add recovery password style

--- front/js/vendor/auth_client.js
+++ front/js/vendor/auth_client.js
@@ -13,6 +13,28 @@ function goToHomePage() {
   window.location.replace("../../html/home_client/home_page.html");
 }
 
+function goToRecoveryCode(){
+  window.location.href = "../../html/auth_client/recovery_code.html";
+}
+
+function goToNewPassword(){
+  window.location.href = "../../html/auth_client/new_password.html";
+}
+
+async function sendLoginAndPassword(log, password){
+  const response = await fetch('', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({login: log, password: password})
+  });
+  const result = await response.json();
+
+}
+
+
+
 const http = new XMLHttpRequest();
 const url = 'cvbnm,';
 http.open("GET", url);
